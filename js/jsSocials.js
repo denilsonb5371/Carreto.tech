729c8ccdb6058449c429be1f94732e709fdb9ecd
Denilson Santos <101111331+denilsonb5371@users.noreply.github.com>
2022-06-03 22:25:30 -0300

    Add files via upload

---
+++ js/jsSocials.js
@@ -0,0 +1,6 @@
+$("#share").jsSocials({    
+    shares: ["email", "twitter", "facebook", "googleplus", "linkedin", "pinterest", "whatsapp"]
+});
+
+$("#share").css('background-color','white').css('padding-left','15px');    
+  
